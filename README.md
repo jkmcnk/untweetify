18d7c06b1999997d6a723f9e9f7b58d0d009e40f
jaKa <jaka@koofr.net>
2022-12-14 13:50:10 +0100

    Added a brief intro and usage description. Added the script itself.

---
+++ README.md
@@ -0,0 +1,71 @@
+# untweetify
+
+Delete each and every tweet you ever tweeted.
+
+Also, do it with bash. Like real women do.
+
+## Prerequisites
+
+- an archive of your twitter data, in the form of a zip, delivered to you by twitter upon request at <https://twitter.com/settings/download_your_data>, unpacked. It's the data/tweets.js and data/tweets-part*.js files that are of interest to us.
+
+- your own custom little twitter app created on the twitter developer dashboard, <https://developer.twitter.com/en/portal/dashboard/>
+  - configure for oauth2 authorization, as a confidential client
+  - set the callback URL to http://localhost:1666/
+  - generated and copy the client ID and client secret
+
+- bash, curl, nc and jq installed on your system
+
+- export the following env variables
+
+```sh
+TW_CLIENT_ID=<your app client ID>
+TW_CLIENT_SECRET=<your app client secret>
+TW_ARCHIVE_PATH=<the path to where the data directory from the archive resides>
+```
+
+## Authenticate
+
+Run
+
+```sh
+bash untweetify.sh auth
+```
+
+It should open a browser (or give you the URL to open manually) with a page where you must authorize the app to access your twitter account. Once the flow finishes without errors, it should inform you that
+
+```txt
+Yay. I am now authenticated and ready to nuke your tweets.
+```
+
+Never mind that your browser probably complained about the redirect page not working. We just didn't bother to send anything back to the browser once we got the authentication data.
+
+## Prepare
+
+Run
+
+```sh
+bash untweetify.sh prep
+```
+
+Once it finishes, you should get the following files in your working directory:
+
+```txt
+pending.txt
+deleted.txt
+```
+
+The latter one being empty, and the former one containing all the IDs of the tweets you ever tweeted.
+
+## Nuke
+
+Run
+
+```sh
+bash untweetify.sh nuke
+```
+
+It will run a long while, as you are only allowed to delete 50 tweets every 15 minutes. Run this in a `screen` session or somesuch.
+
+Once it's done, all the IDs of the tweets deleted are in `deleted.txt`, and there's also a file `failed.txt` that contains the IDs of the tweets that failed to be deleted.
+
+If you preserve the `deleted.txt` file, the next run will only attempt to delete the tweets that are not present in this file.
---
+++ untweetify.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+if [ -z "$TW_CLIENT_ID" -o -z "$TW_CLIENT_SECRET" ]; then
+  echo "You need to set your app client ID and secret via env vars TW_CLIENT_ID and TW_CLIENT_SECRET"
+  exit 1
+fi
+
+BASIC_AUTH=`echo -n "$TW_CLIENT_ID:$TW_CLIENT_SECRET"|base64 -w0`
+
+if [ "$1" = "auth" ]; then
+  touch code.txt
+  chmod 0600 code.txt
+  nc -l localhost 1666 1>code.txt 2>&1 &
+  NCPID=$!
+  CHALLENGE=`dd if=/dev/random bs=16 count=1|base64|sed -re 's/[\/+=]/x/g'`
+  URL="https://twitter.com/i/oauth2/authorize?response_type=code&client_id=$TW_CLIENT_ID&redirect_uri=http://localhost:1666/&scope=tweet.read%20users.read%20tweet.write%20offline.access&state=state&code_challenge=$CHALLENGE&code_challenge_method=plain"
+  echo Visit the following URL in the browser and authorize your untweet app:
+  echo $URL
+  xdg-open "$URL" 1>/dev/null 2>&1
+
+  while true; do
+    if ! grep -E '^GET ' code.txt 1>/dev/null 2>&1; then 
+      sleep 1
+    else
+      CODE=`cat code.txt | grep '^GET ' | sed -re 's/.*&code=([^ &]+).*/\1/'`
+      break
+    fi
+  done
+
+  kill $NCPID
+  if [ -z "$CODE" ]; then
+    echo "Failed to get authorization code."
+    exit 2
+  fi
+
+  rm -f code.txt
+  touch auth.json
+  chmod 0600 auth.json
+  curl --location --request POST 'https://api.twitter.com/2/oauth2/token' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    --data-urlencode "code=$CODE" \
+    --data-urlencode 'grant_type=authorization_code' \
+    --data-urlencode "client_id=$TW_CLIENT_ID" \
+    --data-urlencode 'redirect_uri=http://localhost:1666/' \
+    --data-urlencode "code_verifier=$CHALLENGE" \
+    -H "Authorization: Basic $BASIC_AUTH" >auth.json 2>/dev/null
+
+  if ! grep refresh_token <auth.json 1>/dev/null 2>&1; then
+    echo "Failed to authorize the app."
+    rm -f auth.json
+    exit 3
+  fi
+
+  echo "Yay. I am now authenticated and ready to nuke your tweets."
+  exit 0
+elif [ "$1" = "prep" ]; then
+  if [ -z "$TW_ARCHIVE_PATH" ]; then
+    echo "You need to set env var TW_ARCHIVE_PATH to point to where you unzipped your twitter data archive."
+    exit 1
+  fi
+  rm -f pending.txt deleted.txt
+  touch pending.txt
+  for T in `ls $TW_ARCHIVE_PATH/data/tweets*.js|sort -r`; do
+    echo "Processing tweets from $T"
+    sed -re 's/^window\.YTD\.tweets.part[0-9]+ = //' <$T | jq '.[].tweet.id' | sed -re 's/"//g' >>pending.txt
+  done
+  touch deleted.txt
+  exit 0
+elif [ "$1" = "nuke" ]; then
+  # the delete request rate limit is 50/15min
+  if ! [ -f auth.json ]; then
+    echo "You need to authenticate first."
+    exit 1
+  fi
+  if ! [ -f pending.txt ]; then
+    echo "You need to prepare the data first."
+    exit 2
+  fi
+  touch deleted.txt
+  cat pending.txt | grep -v -f deleted.txt >toremove.txt
+  DELETED=0
+  FAILED=0
+  COUNT=`wc -l toremove.txt`
+  ACCESS_TOKEN=""
+  N=0    
+  # rate limit is 50/15min, do a bit less not to hit it
+  BATCH=45
+  SLEEP=960
+  echo "Removing $COUNT tweets."
+  split -l $BATCH -d toremove.txt toremove-
+  for B in `ls toremove-*`; do
+    echo "Processing batch $B"
+    for ID in `cat $B`; do
+      if [ -z "$ACCESS_TOKEN" ]; then
+        REFRESH_TOKEN=`cat auth.json | jq .refresh_token | sed -re 's/"//g'`
+        curl --request POST 'https://api.twitter.com/2/oauth2/token' \
+          -H 'Content-Type: application/x-www-form-urlencoded' \
+          --data-urlencode "refresh_token=$REFRESH_TOKEN" \
+          --data-urlencode 'grant_type=refresh_token' \
+          --data-urlencode "client_id=$TW_CLIENT_ID" \
+          -H "Authorization: Basic $BASIC_AUTH" >nuauth.json 2>/dev/null
+        if ! grep refresh_token <nuauth.json 1>/dev/null 2>&1; then
+          rm -f nuauth.json
+          echo "Failed to authenicate."
+          exit 3
+        fi
+        mv nuauth.json auth.json
+        ACCESS_TOKEN=`cat auth.json | jq .access_token | sed -re 's/"//g'`
+      fi
+      RS=`curl -s -o /dev/null -w "%{http_code}" --location --request DELETE \
+        "https://api.twitter.com/2/tweets/$ID" \
+        -H "Authorization: Bearer $ACCESS_TOKEN" 2>/dev/null`
+      echo "Deleted $ID: $RS"
+      if [ "$RS" = "200" ]; then
+        echo $ID >>deleted.txt
+        DELETED=$(($DELETED+1))
+      else
+        echo $ID >>failed.txt
+        FAILED=$(($FAILED+1))
+      fi
+      sleep 1
+    done
+    echo "Batch $B done, sleeping for $SLEEP seconds."
+    sleep $SLEEP 
+  done
+  echo "Deleted $DELETED tweets, failed to delete $FAILED tweets."
+  rm -f toremove.txt toremove-*
+  exit 0
+else
+  echo "Usage: untweetify.sh [auth|prep|nuke]"
+  exit 42
+fi
